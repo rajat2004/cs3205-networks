3cee93996e20e914ad7adf01fd6a1de88ff93072
Rajat Singhal <rajatsinghal564@gmail.com>
2020-02-03 12:33:07 +0530

    Initial code

--- assignment2/receiverGBN.py
+++ assignment2/receiverGBN.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-print("Hello from receiver")
+import socket
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Receiver script for Go Back N protocol")
+
+# TODO: Add arguments required 
+
+
+port = 60000
+packet_length = 1024 # bytes
+
+try:
+    recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as err:
+    print("Socket creation error %s" %(err))
+
+host = socket.gethostname()
+print("Host- " + host)
+
+recv_socket.bind(('', port))
+print("Socket bound to port %d" %(port))
+
+message, address = recv_socket.recvfrom(packet_length)
+print(message.decode())
--- assignment2/senderGBN.py
+++ assignment2/senderGBN.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
-print("Hello from sender!")
+import socket
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Sender script for Go Back N protocol")
+
+# TODO: Add arguments required 
+
+
+port = 60000
+ip = "127.0.0.1"
+addr = (ip, port)
+
+packet_length = 1024 # bytes
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as err:
+    print("Socket creation error %s" %(err))
+
+sequence_no = 1
+message = str(sequence_no).ljust(packet_length)
+
+# message = "Hello World".encode()
+s.sendto(bytes(message, "utf-8"), addr)
